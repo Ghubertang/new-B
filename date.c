2ce457afbb2e786bd516bb622ae6e13e28d868ed
唐敬堃 <72055914+Ghubertang@users.noreply.github.com>
2020-11-05 15:02:14 +0800

    Update date.c

--- date.c
+++ date.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main()
 {
@@ -8,13 +7,13 @@ int main()
 	printf("Dated this %d",d);
 	d%=10;
 	switch(d){
-		case 1: printf("st");
+		case 1: case 21: case 31: printf("st");
 		break;
 		
-		case 2: printf("nd");
+		case 2: case 22:printf("nd");
 		break;
 		
-		case 3: printf("rd");
+		case 3: case 23:printf("rd");
 		break;
 		
 		default: printf("th");
@@ -56,88 +55,16 @@ int main()
 	 w = (c / 4) - 2 * c + (y + y / 4) + (13 * (m + 1) / 5) + d - 1;
 	 while (w < 0) w += 7;
 	 w %= 7;
-	 printf("Õâ¸öÈÕÆÚÊÇ£ºÐÇÆÚ");
+	 printf("è¿™ä¸ªæ—¥æœŸæ˜¯ï¼šæ˜ŸæœŸ");
 	 switch(w){
-	 	 case 0:printf("ÈÕ");break;
-		 case 1:printf("Ò»");break; 
-		 case 2:printf("¶þ");break; 
-		 case 3:printf("Èý");break; 
-		 case 4:printf("ËÄ");break; 
-		 case 5:printf("Îå");break; 
-		 case 6:printf("Áù");break;  
+	 	 case 0:printf("æ—¥");break;
+		 case 1:printf("ä¸€");break; 
+		 case 2:printf("äºŒ");break; 
+		 case 3:printf("ä¸‰");break; 
+		 case 4:printf("å››");break; 
+		 case 5:printf("äº”");break; 
+		 case 6:printf("å…­");break;  
 	 }
 	 printf("\n"); 
 	 return 0;
  } 
-=======
-#include<stdio.h>
-int main()
-{
-	int m,d,y,w,c;
-	printf("Enter date(mm/dd/yy):");
-	scanf("%d/%d/%d",&m,&d,&y);	
-	printf("Dated this %d",d);
-	d%=10;
-	switch(d){
-		case 1: printf("st");
-		break;
-		
-		case 2: printf("nd");
-		break;
-		
-		case 3: printf("rd");
-		break;
-		
-		default: printf("th");
-		break;
-	}
-	printf(" day of ");
-	switch(m){
-		case 1:  printf("January");    
-		break;
-		case 2:  printf("February");   
-		break;
-		case 3:  printf("March");      
-		break;
-		case 4:  printf("April");      
-		break;
-		case 5:  printf("May");        
-		break;
-		case 6:  printf("June");       
-		break;
-		case 7:  printf("July");       
-		break;
-		case 8:  printf("August");     
-		break;
-		case 9:  printf("September");  
-		break;
-		case 10: printf("October");    
-		break;
-		case 11: printf("November");   
-		break;
-		case 12: printf("December");  
-		 break;
-	}
-	printf(", 20%.2d.\n",y);
-     if (m ==1 || m == 2) 
-	 {y--;
-	 m += 12;
-	 }
-	c=20;
-	 w = (c / 4) - 2 * c + (y + y / 4) + (13 * (m + 1) / 5) + d - 1;
-	 while (w < 0) w += 7;
-	 w %= 7;
-	 printf("Õâ¸öÈÕÆÚÊÇ£ºÐÇÆÚ");
-	 switch(w){
-	 	 case 0:printf("ÈÕ");break;
-		 case 1:printf("Ò»");break; 
-		 case 2:printf("¶þ");break; 
-		 case 3:printf("Èý");break; 
-		 case 4:printf("ËÄ");break; 
-		 case 5:printf("Îå");break; 
-		 case 6:printf("Áù");break;  
-	 }
-	 printf("\n"); 
-	 return 0;
- } 
->>>>>>> 1bc61b115baae9c1113d0d5c9941ae006651412a
