3d252b76f99703601e5c158f8166dec2f720f9e5
唐敬堃 <72055914+Ghubertang@users.noreply.github.com>
2021-01-11 23:08:58 +0800

    Add files via upload

---
+++ 1021.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[10][20],b[10][20];
+	int num[100];
+	for(int i=0;i<10;i++)
+	{
+		gets(a[i]);
+		gets(b[i]);
+		scanf("%d",num[1]);
+	}
+	for(int i=0;i<10;i++)
+	{
+		puts(a[i]);
+		puts(b[i]);
+	}
+	
+}
